364a8c3cf3f2a5d01248c9f55eaed2b4e8eaa114
shrikanthg98 <shrikanthg18@gmail.com>
2024-01-11 12:48:15 +0530

    major file changes according to YT tutorial

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const Home = () => {
+    return(
+        <View>
+            <Text>Welcome Home</Text>
+        </View>
+    )
+}
+
+export default Home;
