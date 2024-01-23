91b8bd80ca74be0505f37ae8d15aa492a21dc002
DESKTOP-FQ21HS5\dell <shrikanth.g@thoughtclan.com>
2024-01-23 11:53:31 +0530

    connecting the app to rapid apis

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -10,11 +10,17 @@ import styles from './popularjobs.style'
 import { COLORS, SIZES } from '../../../constants'
 import PopularJobCard from '../../common/cards/popular/PopularJobCard'
 import { useRouter } from 'expo-router'
+import useFetch from '../../../hook/useFetch'
 
 const Popularjobs = () => {
   const router = useRouter();
-  const isLoading = false;
-  const error = false;
+  const { data, isLoading, error } = useFetch(
+    'search',
+    {
+      query: 'React Developer',
+      num_pages: 1
+    }
+  )
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -30,8 +36,11 @@ const Popularjobs = () => {
           <Text>Something went wrong</Text>
         ) : (
           <FlatList
-            data={[1, 2, 3, 4, 5]}
+            data={[1, 2, 3, 4, 5, 6, 7, 8]}
             renderItem={({ item }) => (<PopularJobCard item={item} />)}
+            keyExtractor={item => item?.job_id}
+            contentContainerStyle={{ columnGap: SIZES.medium }}
+            horizontal
           />
         )}
       </View>
